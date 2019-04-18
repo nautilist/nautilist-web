a56eeb2dadd59c05e27d6e6d31fd733613c6814c
joeyklee <jsphknglee@gmail.com>
2019-04-18 17:25:11 -0400

    updated main

--- styles/index.js
+++ styles/index.js
@@ -35,7 +35,7 @@ html{
 module.exports = {
     body:`w-100 h-100 code lh-copy flex flex-column`,
     navbarTop:`w-100 h-auto flex flex-column flex-row-ns min-height-60px bg-near-white`,
-    main:`w-100 flex flex-column flex-grow-1 mb4`,
+    main:`w-100 flex flex-column flex-grow-1`,
     footer:`w-100 flex flex-column flex-row-ns min-height-180px bg-near-white`,
     aTag:`link black pointer`,
     modalContainer:`w-100 h-100 fixed top-0 left-0 bg-light-green max-z`,
